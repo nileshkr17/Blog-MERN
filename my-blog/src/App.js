fafd3a491728ab2c1fa2d8bbe8a1ab37dcab1929
Nilesh <ece.20becd76@silicon.ac.in>
2022-12-07 00:35:07 +0530

    [Routes done]

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,22 +1,28 @@
 import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticlesListPage from './pages/ArticlesListPage';
 import ArticlePage from './pages/ArticlePage';
-import NotFoundPage from './pages/NotFoundPage';
+// import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <h1>My Awesome Blog </h1>
       <div id="page-body">
-       <HomePage />
-        <AboutPage />
-        <ArticlesListPage />
-        <ArticlePage />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/articles" element={<ArticlesListPage />} />
+          <Route path="/article" element={<ArticlePage />} />
+          {/* <Route path="*" element={<NotFoundPage />} /> */}
 
+        </Routes>
       </div>
     </div>
+    </BrowserRouter>
   );
 }
 
