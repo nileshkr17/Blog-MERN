fac6b2b5a175da6fbc45d31ffdd753a026dfb480
Nilesh <ece.20becd76@silicon.ac.in>
2022-12-07 01:05:35 +0530

    [added useParams]

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import NavBar from './pages/NavBar';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticlesListPage from './pages/ArticlesListPage';
@@ -10,13 +11,13 @@ function App() {
   return (
     <BrowserRouter>
     <div className="App">
-      <h1>My Awesome Blog </h1>
+      <NavBar/>
       <div id="page-body">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/articles" element={<ArticlesListPage />} />
-          <Route path="/article" element={<ArticlePage />} />
+          <Route path="/article/:articleId" element={<ArticlePage />} />
           {/* <Route path="*" element={<NotFoundPage />} /> */}
 
         </Routes>
--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import articles from './article-content';
+import { useParams } from 'react-router-dom'
+
 
 const ArticlePage = () => {
+  const {articleId} = useParams();
+  const article = articles.find(article=>article.name ===articleId);
+  
   return (
-    <>ArticlePage</>
-  )
+    <>
+    <h1>{article.title}</h1>
+    <h2>{article.name}</h2>
+    <p>{article.content.map(paragraph=>(
+      <p>{paragraph}</p>
+    ))}</p>
+  
+  </>
+  );
 }
 
-export default ArticlePage
+export default ArticlePage;
