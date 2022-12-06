6c58205018c9ca2121b49a1d205739ebecf6f62e
Nilesh <ece.20becd76@silicon.ac.in>
2022-12-07 01:46:23 +0530

    [article with no console err]

---
+++ my-blog/src/components/ArticlesList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import articles from '../pages/article-content'
+import { Link } from 'react-router-dom'
+const ArticlesList = ({articles}) => {
+  return (
+    <>
+    {
+    articles.map(article => (
+      <Link  key={article.name} className="article-list-item" to ={`/article/${article.name}`}>
+        <h3>{article.title}</h3>
+        <p>{article.content[0].substring(0,150)}...</p>
+      </Link>
+    )
+    )}
+    </>
+  )
+}
+
+export default ArticlesList
--- my-blog/src/pages/ArticlesListPage.js
+++ my-blog/src/pages/ArticlesListPage.js
@@ -1,17 +1,13 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import articles from './article-content';
+import ArticlesList from '../components/ArticlesList';
+
+
 const ArticlesListPage = () => {
   return (
     <>
     <h1>Articles</h1>
-    {articles.map(article => (
-      <Link  key={article.name} className="article-list-item" to ={`/article/${article.name}`}>
-        <h3>{article.title}</h3>
-        <p>{article.content[0].substring(0,150)}...</p>
-      </Link>
-    )
-    )}
+    <ArticlesList articles={articles}/>
     </>
   );
 }
