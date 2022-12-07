232885018a415ef79ccc1dbfacde0439390e4b57
Nilesh <ece.20becd76@silicon.ac.in>
2022-12-07 18:35:29 +0530

    [added state]

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -2,10 +2,16 @@ import React from 'react'
 import articles from './article-content';
 import { useParams } from 'react-router-dom'
 import NotFoundPage from './NotFoundPage';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 const ArticlePage = () => {
+  const [articleInfo,setArticleInfo] = useState({upvotes:0,comments:[]});
+
+  useEffect(()=> {
+    setArticleInfo({upvotes:3,comments:[]});
+  });
+
   const {articleId} = useParams();
   const article = articles.find(article=>article.name ===articleId);
 
@@ -17,6 +23,7 @@ const ArticlePage = () => {
     <>
     <h1>{article.title}</h1>
     <h2>{article.name}</h2>
+    <p>This article has {articleInfo.upvotes} upvote(s)</p>
     {article.content.map((paragraph,i)=>(
       <p key={i}>{paragraph}</p>
     ))}
