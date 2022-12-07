5d2a0cb7d0db483bcf5c933b53c3a6b90c252020
Nilesh <ece.20becd76@silicon.ac.in>
2022-12-07 18:29:33 +0530

    [useState-init]

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -2,12 +2,13 @@ import React from 'react'
 import articles from './article-content';
 import { useParams } from 'react-router-dom'
 import NotFoundPage from './NotFoundPage';
+import { useState } from 'react';
 
 
 const ArticlePage = () => {
   const {articleId} = useParams();
   const article = articles.find(article=>article.name ===articleId);
-  
+
   if(!article) {
     return  <NotFoundPage/>
   }
