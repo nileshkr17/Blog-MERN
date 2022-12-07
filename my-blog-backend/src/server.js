ecb9bb524e30ae22d8902953d3d3213d1bef3891
Nilesh <ece.20becd76@silicon.ac.in>
2022-12-07 12:09:04 +0530

    [backend completed]

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,55 +1,65 @@
 import express from 'express';
-//locakhost:8000/articles/{}
-
+import {db,connectToDb} from './db.js';
 const app = express();
 app.use(express.json());
-let articleInfo=[{
-    name: 'learn-react',
-    upvotes: 1,
-    comments: [],
-},
-{
-    name: 'learn-node',
-    upvotes: 0,
-    comments: [],
-},
-{
-    name: 'my-thoughts-on-resumes', 
-    upvotes: 0,
-    comments: [],
-}];
 
-app.put('/api/articles/:name/upvote',(req,res)=>{
-    const {name}=req.params;
-    const article = articleInfo.find((article)=>article.name===name);
+app.get('/api/articles/:name', async(req,res)=>{
+    const {name} =req.params;
+   
+    const article = await db.collection('articles').findOne({name});
     if(article){
-        article.upvotes+=1;
-        res.send(`The ${name} article now has ${article.upvotes} upvotes`);
-        res.status(200).json({message:'Upvote successful',article});
-    } else{
-        res.status(404).json({message:'Article not found'});
+         res.json(article);
+    }else{
+        res.sendStatus(404);
     }
-
-})
-
-app.post('/api/articles/:name/comments',(req,res)=>{
-        const {name}=req.params;    
-        const {postedBy,text}=req.body;
-        const article = articleInfo.find((article)=>article.name===name);
-        if(article){
-            article.comments.push({postedBy,text});
-            res.send(article.comments);
-        }
-        else{
-            res.status(404).json({message:'Article not found'});
-            res.send(`The article named ${name} not found`);
-        }
-        
 
 });
 
 
 
-app.listen(8000,()=>{
-    console.log('Server is listening on port 8000');
+app.put('/api/articles/:name/upvote', async(req,res)=>{
+    const {name}=req.params;
+    
+    await db.collection('articles').updateOne({name},
+        {
+            $inc:{upvotes:1}
+        
+ });
+    //wanna update an article with a given name and increment the upvotes by 1
+   
+        const article = await db.collection('articles').findOne({name});
+    if(article){
+        res.send(`The ${name} article now has ${article.upvotes} upvotes`);
+        res.status(200);
+    } else{
+        res.sendStatus(404);
+    }
+
 })
+
+app.post('/api/articles/:name/comments',async(req,res)=>{
+        const {name}=req.params;    
+        const {postedBy,text}=req.body;
+       
+        await db.collection('articles').updateOne({name},
+            {
+                $push:{comments:{postedBy,text}},
+            });
+            const article = await db.collection('articles').findOne({name});
+        if(article){
+            
+            res.send(article.comments);
+        }
+        else{
+            res.status(404).json({message:'Article not found'});
+            res.send(`The article named ${name} not found`);
+        }    
+});
+
+connectToDb(()=>{
+    console.log('Successfully connected to database!');
+    app.listen(8000,()=>{
+        console.log('Server is listening on port 8000');
+    });
+})
+
